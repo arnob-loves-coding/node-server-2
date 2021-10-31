b08c19fcc2a1d69f82b67579a850f041e2af3746
arnob-loves-coding <arnobroy-13-2018623891@eng.du.ac.bd>
2021-10-31 16:52:10 +0600

    b

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // import
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 
@@ -11,8 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // database connect
-const uri =
-  "mongodb+srv://Arnob:6vSMAqqvQzhZFkxK@cluster0.gopne.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gopne.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
